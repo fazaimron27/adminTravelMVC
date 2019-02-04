5a6c69978851fc993a921fa0e39f37f4a17d2b5e
fazaimron27 <cincaumyself@gmail.com>
2019-02-04 13:17:15 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About {
-	public function index($nama = 'Faza Iman Imron', $pekerjaan = 'siswa SMKN 1 BANYUWANGI jurusan RPL kelas 11')
+class About extends Controller {
+	public function index($nama = 'Faza Iman Imron', $pekerjaan = 'siswa SMKN 1 BANYUWANGI jurusan RPL kelas 11', $umur = '17')
 	{
-		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+		$data['nama'] = $nama;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['umur'] = $umur;
+		$data['judul'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 
 	public function page()
 	{
-		echo ' About/page';
+		$data['judul'] = 'pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
